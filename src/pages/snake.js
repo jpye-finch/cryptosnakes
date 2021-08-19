0a5e324ade86d72b0a083f5ae03335da286013ff
jpf <hello@pye-finch.co.uk>
2021-08-19 17:11:14 +0100

    snake data showing

--- src/pages/snake.js
+++ src/pages/snake.js
@@ -3,17 +3,18 @@ import Layout from '../components/layout.js';
 import { StaticImage } from 'gatsby-plugin-image'
 
 
-const SnakePage = () => {
+const SnakePage = ({location}) => {
     return (
         <Layout>
+            
         <div class="dark:bg-gray-800 flex relative z-20 items-center overflow-hidden">
                  <div class="container mx-auto px-6 flex relative py-16">
               <div class="sm:w-2/3 lg:w-3/6 flex flex-col relative z-20">
                <p class="mb-2 text-sm sm:text-base text-gray-700 dark:text-white">
-            SmartSnake  
+
              </p>
             <h1 class="normal-case text-4xl sm:text-4xl font-black flex flex-col leading-none dark:text-white text-gray-800">
-                Name of snake
+                {location.state.snakes.name}
             </h1>
         </div>
         <div class="sm:block sm:w-1/3 lg:w-3/6 relative">
@@ -25,5 +26,5 @@ const SnakePage = () => {
         </Layout>  
     )
   }
-  
+
   export default SnakePage
